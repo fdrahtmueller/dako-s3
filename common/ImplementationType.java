bcaa0c667089a252d4dce4f7ba1eece8729b799c
fdrahtmueller <florian.drahtmueller@googlemail.com>
2017-11-27 13:38:36 +0100

    Add files via upload
    src/main/java/edu/hm/dako/chat
    Upload der Klassen des Java Projekts

---
+++ common/ImplementationType.java
@@ -0,0 +1,12 @@
+package edu.hm.dako.chat.common;
+
+/**
+ * Implementierungsvarianten des Lasttests mit verschiedenen Transportprotokollen
+ *
+ * @author Mandl
+ */
+
+public enum ImplementationType {
+	TCPSimpleImplementation,
+	TCPAdvancedImplementation,
+}
---
+++ server/AdvancedChatServerImpl.java
@@ -0,0 +1,26 @@
+package edu.hm.dako.chat.server;
+
+import java.util.concurrent.ExecutorService;
+
+import edu.hm.dako.chat.connection.ServerSocketInterface;
+
+public class AdvancedChatServerImpl implements ChatServerInterface {
+
+	public AdvancedChatServerImpl(ExecutorService newCachedThreadPool, ServerSocketInterface decoratedServerSocket,
+			ChatServerGuiInterface serverGuiInterface) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
